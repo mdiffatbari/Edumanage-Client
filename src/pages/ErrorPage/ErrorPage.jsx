a3142f33bb14073e9a081d4d0e33a1e537eaa6f0
mdiffatbari <iffatbaribappy1999@gmail.com>
2025-07-20 14:58:51 +0600

    errorpage design done

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import errorJPG from '../../assets/error.jpg'
+import { Link } from 'react-router';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>Page not found</h1>
+        <div className='w-11/12 mx-auto text-center'>
+            <img className='mx-auto w-3xl' src={errorJPG} alt="" />
+            <Link to='/'><a role="button" className="btn bg-[#cb3f02] text-white">Back to Home</a></Link>
         </div>
     );
 };
