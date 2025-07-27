ff25c0e3517fb1c479571466fb7a5a283cf7a01c
mdiffatbari <iffatbaribappy1999@gmail.com>
2025-07-28 02:31:58 +0600

    dashboard layout function done

--- src/pages/Dashboard/DashboardWelcome/DashboardWelcome.jsx
+++ src/pages/Dashboard/DashboardWelcome/DashboardWelcome.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { FaRegSmileBeam } from 'react-icons/fa';
+import logo from "../../../assets/logo.png"
 
 const DashboardWelcome = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] text-center px-4">
-      <FaRegSmileBeam className="text-6xl text-orange-500 mb-4 animate-bounce" />
       <h1 className="text-3xl md:text-5xl font-bold text-gray-800 mb-2">
+        <img
+        className='animate-bounce mb-4 w-2/5 mx-auto'
+         src={logo} alt="" />
         Welcome to your Dashboard
       </h1>
-      <p className="text-lg md:text-xl text-gray-600">
+      <p className="text-lg md:text-xl text-[#cb3f02]">
         Please select a menu item from the sidebar to continue.
       </p>
     </div>
