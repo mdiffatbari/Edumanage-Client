852e3ce7761548e612c661b8d81d7e0520a2136c
Md Iffat Bari <88897305+mdiffatbari@users.noreply.github.com>
2025-08-17 15:37:46 +0600

    Update firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,19 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC3rvYYZPmY0LThcfuiFPpjPCMqYk9DC9s",
-  authDomain: "edumanage-3629e.firebaseapp.com",
-  projectId: "edumanage-3629e",
-  storageBucket: "edumanage-3629e.firebasestorage.app",
-  messagingSenderId: "19832434469",
-  appId: "1:19832434469:web:84c1152fb75b2ddbc7b4cc"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export default app;
